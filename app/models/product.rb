bbfeec9da16feebba2826b61a58a422bcf1e3956
sdsog <34170634+sdsog@users.noreply.github.com>
2019-07-12 02:29:01 +0000

    adds working schema and database for reviews

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,14 @@
-class Product < ActiveRecord::Base
+# frozen_string_literal: true
 
+class Product < ActiveRecord::Base
   monetize :price_cents, numericality: true
   mount_uploader :image, ProductImageUploader
 
   belongs_to :category
+  has_many :reviews
 
   validates :name, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
   validates :category, presence: true
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  has_many :reviews
+
   validates_uniqueness_of :email, case_sensitive: false, presence: true
   validates :password, presence: true
   validates :password, length: { minimum: 3 }
