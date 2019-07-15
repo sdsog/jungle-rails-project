0c2ff76aab57057ef5af5853617af306b89d0e30
sdsog <34170634+sdsog@users.noreply.github.com>
2019-07-15 20:15:30 +0000

    adds testing to product creation

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  before(:each) do
+    @category = Category.create name: 'TEST CATEGORY'
+  end
+
+  describe 'Validations' do
+    it 'creates a new product' do
+      @product = Product.create name: 'name', price_cents: 1, quantity: 1, category: @category
+      expect(@product).to be_valid
+    end
+
+    it 'a product should require a name' do
+      @product = Product.create name: nil, price_cents: 1, quantity: 1, category: @category
+      expect(@product).to_not be_valid
+    end
+
+    it 'a product should have a quantity' do
+      @product = Product.create name: 'name', price_cents: 1, quantity: nil, category: @category
+      expect(@product).to_not be_valid
+    end
+
+    it 'a product should have a price' do
+      @product = Product.create name: 'name', price_cents: nil, quantity: 1, category: @category
+      expect(@product).to_not be_valid
+    end
+
+    it 'a product should have a category' do
+      @product = Product.create name: 'name', price_cents: 1, quantity: 1, category: nil
+      expect(@product).to_not be_valid
+    end
+  end
+end
